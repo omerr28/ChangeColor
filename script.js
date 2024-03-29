4d75d98fe8da217c9a1eeb20967d660b1ccc435f
omerr28 <155801710+omerr28@users.noreply.github.com>
2024-03-29 13:01:49 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const btn = document.getElementById('changeColor');
+let randomNumber = () => {
+    let randomNumber = (math.floor(math.randomNumber)*256)
+    return randomNum ;
+
+}
+let changeColor = () => {
+    let randomColor = `rgb($(randomNumber())},rgb($(randomNumber())},rgb($(randomNumber())}`
+    
+}
